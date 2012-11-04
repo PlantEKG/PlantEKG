6d98248cc3f044e64bb3c69e8bdc2af69fe66761
Allen Zeng <allenz456@gmail.com>
2012-11-04 19:44:22 +0000

    added index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>PlantEKG</title>
+
+<!-- CSS FILE -->
+<!-- <link type="text/css" rel="stylesheet" href="main.css"> -->
+<!-- JAVASCRIPT FILE -->
+<!-- <script type="text/javascript" src="main.js"></script> -->
+<!-- <script type="text/javascript" src="slider.js"></script> -->
+<!-- JQuery for swipe -->
+<!-- <script src="jquery_mobile/jquery.js"></script> -->
+
+<!-- JQuery Mobile -->
+<!-- <link rel="stylesheet" href="jquery_mobile/jquery.mobile-1.2.0.css" /> -->
+<!-- <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script> -->
+<!-- <script src="jquery_mobile/jquery.mobile-1.2.0.js"></script> -->
+
+
+<!-- <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable:no;"> -->
+
+</head>
+<body id='body_div'>
+
+	<!-- HEADER -->
+	<div id='title'>PlantEKG</div>
+
+	<div id='test'></div>
+
+	<!-- Displays all plants belonging to the collection of the user -> Pulls information from the collection table-->
+	<div id='My Plants'>Will display plants here</div>
+
+	<div class='add_plant_button'>
+	<button type="button" onclick="alert('Hello world!')">Add New Plant to My Collection</button>
+	<div>
+
+
+</body>
+</html>
