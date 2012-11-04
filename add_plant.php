cba0316737a6cb91eb45dd693a3769290c3bbb2f
Tommy Yu <jiayu2014@u.northwestern.edu>
2012-11-04 21:11:39 +0000

    can add plants to db

---
+++ add_plant.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+</head>
+<body>
+<form method="POST" action="add_plant_to_database.php" method="POST" target="add_room_target">\
+			Plant Name: <input type="textbox" name="plant_name"><br>\
+			Spacing: <input type="textbox" name="spacing"><br>\
+			Feed: <input type="textbox" name="feed"><br>\
+			Water: <input type="textbox" name="water"><br>\
+			Preferred Light: <input type="textbox" name="preferred_light"><br>\
+			Plant url: <input type="textbox" name="plant_url"><br>\
+			<input type="submit" value="Submit">\
+		</form>\
+	</body>
+	</html>
