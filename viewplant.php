76c6ee60400db268709c430636d0c7264aa6cd09
Tommy Yu <jiayu2014@u.northwestern.edu>
2012-11-06 19:52:18 +0000

    viewplant changes

--- viewplant.php
+++ viewplant.php
@@ -33,21 +33,31 @@
 	$plant_table_name='plants';
 
 	$plant_data_query = mysql_query("SELECT * FROM " . $collection_table_name . " join " . $plant_table_name . " WHERE user_id='" . $_GET['user_id'] . "'");
+	$plant_data_query2 = mysql_query("SELECT * FROM " . $plant_table_name . "WHERE plant_id = '1'");
 	$plant_data = mysql_fetch_array($plant_data_query);
+
+	$plant_data_array = array();
+
+	while($collection_data_hold = mysql_fetch_array($plant_data_search))
+	{
+		array_push($plant_data_array, $collection_data_hold);
+	}
+
 
 	// print_r($plant_data);
 
-	echo displayPlant($plant_data);
+	// echo displayPlant($plant_data);
 
-	function displayPlant($plant_data_array)
-	{
-		echo "Plant name: " . $plant_data_array['plant_name'];
-		echo "<br><img src='" . $plant_data_array['plant_url'] . "'>";
-		echo "<br>Spacing: " . $plant_data_array['spacing'] . "";
-		echo "<br>Water: " . $plant_data_array['water'] . "";
-		echo "<br>Preferred Light: " . $plant_data_array['preferred_light'] . "";
-		echo "<br><br>";
-	}
+	// function displayPlant($plant_data_array)
+	// {
+		echo $plant_data_array[1][2];		
+		// echo "Plant name: " . $plant_data_array['plant_name'];
+		// echo "<br><img src='" . $plant_data_array['plant_url'] . "'>";
+		// echo "<br>Spacing: " . $plant_data_array['spacing'] . "";
+		// echo "<br>Water: " . $plant_data_array['water'] . "";
+		// echo "<br>Preferred Light: " . $plant_data_array['preferred_light'] . "";
+		// echo "<br><br>";
+	// }
 
 	?>
 
