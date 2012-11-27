92314239308af48d09d6ef4ec9afc2e6903765a2
alexwilson2012 <alexwilson2012@u.northwestern.edu>
2012-11-27 20:51:25 +0000

    mailform now sends mail only if there are plants that need watering and also updates the last/next watering dates

--- mailform.php
+++ mailform.php
@@ -5,7 +5,7 @@ session_start();
   $user_id = $_SESSION['collection_user'];
 
   // $to = $_REQUEST['email'];
-  $to = "brianambielli2007@u.northwestern.edu";
+  $to = "allenzeng2014@u.northwestern.edu";
   $date = date('Y-m-d');
 
   // Open connection to DB
@@ -25,29 +25,38 @@ session_start();
 
   // 11 is the index for the picture
   $numberOfPlants = count($collection_data_array);
-  // $fakeDate = date('2012-11-22');
+  $fakeDate = date('2012-12-03');
 
-  $waterInfo = "You have to water the following plants TODAY (" . $date . "): \r\n ";
+  $waterDateInfo = "You have to water the following plants TODAY (" . $date . "): \r\n ";
+  $waterInfo = "";
   if($numberOfPlants > 0) 
   {
     for ($ii = 0; $ii < $numberOfPlants; $ii++ ) 
     {
       // Name of plant 
-      echo "<h3>" . $collection_data_array[$ii][7] . "</h3>";
-      echo "<dt> Plant Information </dt>" . "<dd>" .$collection_data_array[$ii][5] . "</dd>";
-      echo "<dt> Next Watering Date: </dt>" . "<dd>" . $collection_data_array[$ii][6] . "</dd>";
+      // echo "<h3>" . $collection_data_array[$ii][7] . "</h3>";
+      // echo "<dt> Plant Information </dt>" . "<dd>" .$collection_data_array[$ii][5] . "</dd>";
+      // echo "<dt> Next Watering Date: </dt>" . "<dd>" . $collection_data_array[$ii][6] . "</dd>";
+
       if ($date == $collection_data_array[$ii][3])
       {
         $waterInfo .= $collection_data_array[$ii][7] . " with description " . $collection_data_array[$ii][5] . "\r\n ";
+
+        // Updates the next water date for the current plant
+        $water_frequency = intval($collection_data_array[$ii][21]);
+        $lastWaterDate = $collection_data_array[$ii][3];
+        $newWaterDate = date('Y-m-d', strtotime($collection_data_array[$ii][3]. ' + ' . $water_frequency . ' days'));
+        mysql_query("UPDATE collection set last_water_date='" . $lastWaterDate . "' where collection_plant_id='" . $collection_data_array[$ii][6] ."'");
+        mysql_query("UPDATE collection set next_water_date='" . $newWaterDate . "' where collection_plant_id='" . $collection_data_array[$ii][6] ."'");
       } 
     }
   }
 
-  // echo $waterInfo;
-  // echo $date;
-  // echo $fakeDate;
+// echo $waterInfo;
 
-
+// Send mail only if there are plants that have to be watered today 
+if ($waterInfo != "")
+{
   $headers .= "Reply-To: PlantEKG <plantekg@gmail.com>\r\n"; 
   $headers .= "Return-Path: PlantEKG <plantekg@gmail.com>\r\n"; 
   $headers .= "From: PlantEKG <plantekg@gmail.com>\r\n";
@@ -57,13 +66,10 @@ session_start();
   $headers .= "X-Priority: 3\r\n";
   $headers .= "X-Mailer: PHP". phpversion() ."\r\n";
   $subject = "Time to water your plants!";
-  $message = "This is a reminder from PlantEKG for user " . $user_id . "\r\n " . $waterInfo;
+  $message = "This is a reminder from PlantEKG for user " . $user_id . "\r\n " . $waterDateInfo . $waterInfo;
   mail($to, $subject, $message, $headers);
-
-  //echo $to;
+}
 
   header("Location: http://ec2-107-20-111-184.compute-1.amazonaws.com/brian/PlantEKG/index.php?id=" . $user_id . "",TRUE,303);
-
-
 
 ?>
