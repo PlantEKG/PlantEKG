13f846069263558a1abcc18c1ec46b4bf4616b7c
alexwilson2012 <alexwilson2012@u.northwestern.edu>
2012-12-06 01:46:56 +0000

    fixed font sizes and spacing on the sign up page

--- adduser.php
+++ adduser.php
@@ -20,15 +20,18 @@
 <br><br><br><br><br><br><br><br>
 <form method="POST" name="addUser" action="add_user_to_database.php" method="POST">
 		<fieldset>
-			<legend>Create New Account</legend>
-			<label for='username' >Username:</label>
-			<input type='text' name='name' id='name'/>
-			<label for='email' >Email:</label>
-			<input type='text' name='email' id='email'/>
-			<label for='password' >Password:</label>
-			<input type='text' name='password' id='password'/>
-			<br>Notification Time<br>
-			 Hour:<select name = "hour" style="width: 60px">
+			<legend style='font-size:35px;'>Create New Account</legend>
+			<label for='username' style='font-size:25px;'>Username:</label>
+			<input type='text' name='name' id='name' style='height:50px; width:300px;'/>
+      <br>
+      <br>
+			<label for='email' style='font-size:25px;'>Email:</label>
+			<input type='text' name='email' id='email' style='height:50px; width:300px;'/>
+      <br><br>
+			<label for='password' style='font-size:25px;'>Password:</label>
+			<input type='text' name='password' id='password' style='height:50px; width:300px;'/>
+			<h3>Notification Time</h3>
+			 Hour:<select name = "hour" style='width: 75px;'>
                <option value = 1>1</option>
                <option value = 2>2</option>
                <option value = 3>3</option>
@@ -42,17 +45,17 @@
                <option value = 11>11</option>
                <option value = 12>12</option>
              </select>
-			  Min:<select name = "min" style="width: 60px">
+			  Min:<select name = "min" style='width: 75px;'>
                <option value = 00>00</option>
                <option value = 15>15</option>
                <option value = 30>30</option>
                <option value = 45>45</option>
              </select>
-             AM/PM:<select name = "AMPM" style="width: 60px">
+             AM/PM:<select name = "AMPM" style='width: 90px;'>
                <option value = "AM">AM</option>
                <option value = "PM">PM</option>
            	</select>
-           	<br>
+           	<br><br>
 			<input type='submit' class='btn btn-large' name='Submit' value='Submit' />
 		</fieldset>
 </form>
